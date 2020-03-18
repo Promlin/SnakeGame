1762c2549f0fb46a88e132d5b98eee911d272cf8
Irina <irinatyumyantseva@gmail.com>
2020-03-18 22:19:55 +0300

    final commit

--- SnakeGame/Snake/Program.cs
+++ SnakeGame/Snake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Snake
@@ -10,7 +11,45 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.ReadLine();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize(80, 25);
+
+            Walls walls = new Walls(80, 25);
+            walls.Drow();
+
+
+            Point p1 = new Point(5, 5, '*');
+            Snake snake = new Snake(p1, 4, Direction.RIGHT);
+            snake.Drow();
+            
+            FoodCreator foodCreator = new FoodCreator(80, 25, '#');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
         }
     }
 }
